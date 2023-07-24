1a4fa8bb3d904a7f9d03e5ea32a433052ef53d2c
RanaHisham18 <hishamrana00@gmail.com>
2023-07-23 23:19:04 -0600

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 void main() {
   runApp(MaterialApp(
@@ -12,38 +15,65 @@ void main() {
   ));
 }
 
-class ImageHolder extends StatelessWidget {
+class ImageHolder extends StatefulWidget {
+  const ImageHolder({super.key});
+
+  @override
+  State<ImageHolder> createState() => _ImageHolderState();
+}
+
+class _ImageHolderState extends State<ImageHolder> {
+  int leftImageNumber = 1;
+  int rightImageNumber = 2;
+
+  //function to generate random number
+  void changeImageNumber() {
+    leftImageNumber = Random().nextInt(8) + 1;
+    rightImageNumber = Random().nextInt(8) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          'Try Again',
-          style: TextStyle(color: Colors.indigo, fontSize: 50),
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                // ignore: prefer_const_constructors
-                child: TextButton(
-                  onPressed: (){},
-                  child: Image(
-                    image: AssetImage('images/image-1.png'),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Image(image: AssetImage('images/image-1.png')),
-              ),
-            ),
-          ],
-        ),
-      ],
+       Text(
+        leftImageNumber == rightImageNumber ? 'Congrats, You won!'
+            : 'Try Again',
+      style: TextStyle(color: Colors.indigo, fontSize: 50),
+    ),
+    Row(
+    children: [
+    Expanded(
+    child: Padding(
+    padding: EdgeInsets.all(15),
+    // ignore: prefer_const_constructors
+    child: TextButton(
+    onPressed: () {
+    setState(() {
+    changeImageNumber();
+    });
+
+    },
+    child: Image.asset('images/image-$leftImageNumber.png'),
+    ),
+    ),
+    ),
+
+    Expanded(
+    child: TextButton(
+    onPressed: () {
+    setState(() {
+    changeImageNumber();
+    });
+    },
+    child: Padding(    padding: EdgeInsets.all(15),
+        child: Image.asset('images/image-$rightImageNumber.png')),
+    ),
+    ),
+    ],
+    ),
+    ],
     );
-  }
+    }
 }
+
